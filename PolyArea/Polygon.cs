d0548d49ea3818333d4aa75b6834744c0231ad33
Gemryl Laxina <applewarriorpear@gmail.com>
2020-01-31 09:25:26 -0500

    Added Getsides to Polygon

--- PolyArea/Polygon.cs
+++ PolyArea/Polygon.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace PolyArea
 {
-    public class Point
+    /*public class Point
     {
         public float X { get; set; }
         public float Y { get; set; }
@@ -15,9 +15,61 @@ namespace PolyArea
             X = x;
             y = y;
         }
+    }*/
+    public enum PolygonType
+    {
+        Concave,
+        Convex,
     }
+
+    static class PolygonAnalyzer
+    {
+        internal static PolygonType Classify (Polygon polygon)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Vector2[] GetSides(Polygon polygon)
+        {
+            Vector2[] Sides = new Vector2[polygon.Vertices.Length];
+            int index = 0;
+            foreach (var vertex in polygon.Vertices)
+            {
+                Vector2 vectorA = polygon.Vertices[index];
+                Vector2 vectorB;
+                if (index + 1 == polygon.Vertices.Length)
+                {
+                    //Loop to start
+                    vectorB = polygon.Vertices[0];
+                }
+                else
+                {
+                    vectorB = polygon.Vertices[index + 1];
+                }
+                Sides[index] = vectorB - vectorA;
+                index++;
+            }
+            return Sides;
+        }
+    }
+
     public class Polygon
     {
-       
+        readonly public Vector2[] Vertices;
+        readonly public Vector2[] Sides;
+        readonly public PolygonType Type;
+
+        public Polygon (Vector2[] vertices)
+        {
+            if (vertices.Length < 2)
+            {
+                Console.WriteLine("A Polygon needs more than 2 vertices");
+                throw new IndexOutOfRangeException();
+            }
+            Vertices = vertices;
+            Sides = PolygonAnalyzer.GetSides(this);
+            Type = PolygonAnalyzer.Classify(this);
+        }
+
     }
 }
---
+++ PolygonTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+using PolyArea;
+
+namespace PolygonTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polygon p = new Polygon(
+                new Vector2[]
+                {
+                    new Vector2(0,0),
+                    new Vector2(0,1),
+                    new Vector2(1,1),
+                    new Vector2(1,0),
+
+                }
+                );
+            Console.WriteLine(p.Sides);
+        }
+    }
+}
