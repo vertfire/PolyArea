7fcc5b684655a876cd808277e4c2d49b264f66be
Gemryl Laxina <applewarriorpear@gmail.com>
2020-01-31 08:53:43 -0500

    Created Point class

---
+++ PolyArea/Polygon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace PolyArea
+{
+    public class Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Point (float x, float y)
+        {
+            X = x;
+            y = y;
+        }
+    }
+    public class Polygon
+    {
+       
+    }
+}
